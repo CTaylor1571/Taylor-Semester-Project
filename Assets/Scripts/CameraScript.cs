1ef28a2e24d948bc7cd1735e97df8078996a7e57
CTaylor1571 <90778109+CTaylor1571@users.noreply.github.com>
2023-10-30 08:16:59 -0500

    Update CameraScript.cs
    Changed CamperaScript.cs so that camera stops moving down with the player underneath the map past a certain point. Falling off of the stage does not kill the player yet.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    [SerializeField]
-    GameObject player; 
 
+    GameObject player; 
     GameObject bottom;
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player");
         bottom = GameObject.Find("World Bottom");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if ( player.transform.y <= bottom.transform.y) { }
+        if ( player.transform.position.y <= bottom.transform.position.y) 
+        {
+            transform.SetPositionAndRotation(new Vector3(player.transform.position.x, bottom.transform.position.y,-10), Quaternion.identity);
+        }
     }
 }
